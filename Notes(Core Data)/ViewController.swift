57a4ad2afbbfb3cd874c02531937df0c37aa9981
sunilkr123 <sunilkumarsah6@gmail.com>
2025-09-13 22:53:12 +0530

    initial commit

---
+++ Notes(Core Data)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes(Core Data)
+//
+//  Created by Aman  on 13/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Notes+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Notes+CoreDataClass.swift
+//  
+//
+//  Created by Aman  on 13/01/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Notes: NSManagedObject {
+
+}
